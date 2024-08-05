4c0ed4db600def474d68991a1321657cda923775
João Pedro  Jalles <104013326+joaopjalles@users.noreply.github.com>
2024-08-05 14:39:59 -0300

    Add files via upload

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/domain/filime/DadosAlteracaoFilmes.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatch.domain.filime;
+
+public record DadosAlteracaoFilmes(Long id, String nome, Integer duracao, Integer ano, String genero) {
+}
---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/domain/filime/FilmeRepository.java
@@ -0,0 +1,6 @@
+package br.com.alura.screenmatch.domain.filime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+}
